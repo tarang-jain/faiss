f4d05872759fb412ee671b2b4b03c1dd9c4eb82a
Tarang Jain <jaintarang2015@gmail.com>
2024-01-03 00:32:59 -0800

    dbg

--- faiss/gpu/GpuIndexIVFPQ.cu
+++ faiss/gpu/GpuIndexIVFPQ.cu
@@ -413,8 +413,9 @@ void GpuIndexIVFPQ::train(idx_t n, const float* x) {
                 subQuantizers_ * pq.dsub * utils::pow2(bitsPerCode_),
                 resources_->getDefaultStream(config_.device));
         raft_handle.sync_stream();
+        raft::neighbors::ivf_pq::serialize(raft_handle, "/raid/tarangj/datasets/deep-image-96-inner/index/faiss_trained_index", raft_ivfpq_index);
         raftIndex_->setRaftIndex(std::move(raft_ivfpq_index));
-    } else
+            } else
 #else
     if (config_.use_raft) {
         FAISS_THROW_MSG(
